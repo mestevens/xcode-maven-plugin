68b6e13c3d973f43c79f3da7ecc351d651830c24
Matthew Stevens <mstevens@ea.com>
2014-09-25 15:01:01 -0400

    Updating to build universal frameworks

--- src/main/java/ca/mestevens/ios/XcodeBuildMojo.java
+++ src/main/java/ca/mestevens/ios/XcodeBuildMojo.java
@@ -1,12 +1,10 @@
 package ca.mestevens.ios;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+
+import ca.mestevens.ios.utils.ProcessRunner;
 
 /**
  * Goal which generates your framework dependencies in the target directory.
@@ -44,29 +42,36 @@ public class XcodeBuildMojo extends AbstractMojo {
 	 * @required
 	 */
 	public String targetDirectory;
+	
+	/**
+	 * @parameter property="xcode.framework.name" default-value="${project.artifactId}"
+	 * @readonly
+	 * @required
+	 */
+	public String frameworkName;
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		try {
-			ProcessBuilder processBuilder = new ProcessBuilder(xcodebuild, "-project", xcodeProject, "-scheme", xcodeScheme,
-					"-destination", "generic/platform=iOS", "CONFIGURATION_BUILD_DIR=" + targetDirectory, "archive");
-			processBuilder.redirectErrorStream(true);
-			Process process = processBuilder.start();
-			BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			String line = null;
-			while ((line = input.readLine()) != null) {
-				getLog().info(line);
-			}
-			int returnValue = process.waitFor();
-			
-			if (returnValue != 0) {
-				throw new MojoFailureException("Failed to build project.");
-			}
-		} catch (IOException e) {
-			getLog().error(e.getMessage());
-			throw new MojoFailureException(e.getMessage());
-		} catch (InterruptedException e) {
-			getLog().error(e.getMessage());
-			throw new MojoFailureException(e.getMessage());
+		ProcessRunner processRunner = new ProcessRunner(getLog());
+		int returnValue = processRunner.runProcess(null, xcodebuild, "-project", xcodeProject, "-scheme", xcodeScheme,
+				"-sdk", "iphonesimulator", "CONFIGURATION_BUILD_DIR=" + targetDirectory + "/iphonesimulator", "build");
+		checkReturnValue(returnValue);
+		returnValue = processRunner.runProcess(null, xcodebuild, "-project", xcodeProject, "-scheme", xcodeScheme,
+				"-sdk", "iphonesimulator", "-arch", "x86_64", "CONFIGURATION_BUILD_DIR=" + targetDirectory + "/iphonesimulator6", "build");
+		checkReturnValue(returnValue);
+		returnValue = processRunner.runProcess(null, xcodebuild, "-project", xcodeProject, "-scheme", xcodeScheme,
+				"-sdk", "iphoneos", "CONFIGURATION_BUILD_DIR=" + targetDirectory + "/iphoneos", "build");
+		checkReturnValue(returnValue);
+		returnValue = processRunner.runProcess(targetDirectory, "lipo", "-create", "-output", frameworkName, 
+				"iphonesimulator/" + frameworkName + ".framework/" + frameworkName, "iphonesimulator6/" + frameworkName + ".framework/" + frameworkName,
+				"iphoneos/" + frameworkName + ".framework/" + frameworkName);
+		checkReturnValue(returnValue);
+		processRunner.runProcess(targetDirectory, "cp", "-r", "iphoneos/" + frameworkName + ".framework", ".");
+		processRunner.runProcess(targetDirectory, "cp", frameworkName, frameworkName + ".framework/.");
+	}
+	
+	protected void checkReturnValue(int returnValue) throws MojoFailureException {
+		if (returnValue != 0) {
+			throw new MojoFailureException("Failed to build project.");
 		}
 	}
 
