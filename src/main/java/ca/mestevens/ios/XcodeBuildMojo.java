483035b939ad879017179878102ad20fd70d09ed
Matthew Stevens <mstevens@ea.com>
2014-12-11 17:22:15 -0500

    Adding a flag to choose if you want to build with a 64bit component

--- src/main/java/ca/mestevens/ios/XcodeBuildMojo.java
+++ src/main/java/ca/mestevens/ios/XcodeBuildMojo.java
@@ -1,5 +1,8 @@
 package ca.mestevens.ios;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -49,21 +52,38 @@ public class XcodeBuildMojo extends AbstractMojo {
 	 * @required
 	 */
 	public String frameworkName;
+	
+	/**
+	 * @parameter property="xcode.build.64bit" default-value=true
+	 * @readonly
+	 * @required
+	 */
+	public boolean build64bit;
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		ProcessRunner processRunner = new ProcessRunner(getLog());
 		int returnValue = processRunner.runProcess(null, xcodebuild, "-project", xcodeProject, "-scheme", xcodeScheme,
 				"-sdk", "iphonesimulator", "CONFIGURATION_BUILD_DIR=" + targetDirectory + "/iphonesimulator", "build");
 		checkReturnValue(returnValue);
-		returnValue = processRunner.runProcess(null, xcodebuild, "-project", xcodeProject, "-scheme", xcodeScheme,
-				"-sdk", "iphonesimulator", "-arch", "x86_64", "CONFIGURATION_BUILD_DIR=" + targetDirectory + "/iphonesimulator6", "build");
-		checkReturnValue(returnValue);
+		if (build64bit) {
+			returnValue = processRunner.runProcess(null, xcodebuild, "-project", xcodeProject, "-scheme", xcodeScheme,
+					"-sdk", "iphonesimulator", "-arch", "x86_64", "CONFIGURATION_BUILD_DIR=" + targetDirectory + "/iphonesimulator6", "build");
+			checkReturnValue(returnValue);
+		}
 		returnValue = processRunner.runProcess(null, xcodebuild, "-project", xcodeProject, "-scheme", xcodeScheme,
 				"-sdk", "iphoneos", "CONFIGURATION_BUILD_DIR=" + targetDirectory + "/iphoneos", "build");
 		checkReturnValue(returnValue);
-		returnValue = processRunner.runProcess(targetDirectory, "lipo", "-create", "-output", frameworkName, 
-				"iphonesimulator/" + frameworkName + ".framework/" + frameworkName, "iphonesimulator6/" + frameworkName + ".framework/" + frameworkName,
-				"iphoneos/" + frameworkName + ".framework/" + frameworkName);
+		List<String> lipoCommand = new ArrayList<String>();
+		lipoCommand.add("lipo");
+		lipoCommand.add("-create");
+		lipoCommand.add("-output");
+		lipoCommand.add(frameworkName);
+		lipoCommand.add("iphonesimulator/" + frameworkName + ".framework/" + frameworkName);
+		if (build64bit) {
+			lipoCommand.add("iphonesimulator6/" + frameworkName + ".framework/" + frameworkName);
+		}
+		lipoCommand.add("iphoneos/" + frameworkName + ".framework/" + frameworkName);
+		returnValue = processRunner.runProcess(targetDirectory, lipoCommand.toArray(new String[lipoCommand.size()]));
 		checkReturnValue(returnValue);
 		processRunner.runProcess(targetDirectory, "cp", "-r", "iphoneos/" + frameworkName + ".framework", ".");
 		processRunner.runProcess(targetDirectory, "cp", frameworkName, frameworkName + ".framework/.");
